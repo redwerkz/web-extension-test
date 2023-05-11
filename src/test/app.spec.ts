171d95d5e478de05345af0678f7d5fa2dbce88c2
Rychart Redwerkz <redwerkz@users.noreply.github.com>
2023-05-11 05:22:06 +0200

    Initial commit

---
+++ src/test/app.spec.ts
@@ -0,0 +1,25 @@
+import { createSandbox } from 'sinon';
+import { App } from '../lib/app';
+import * as Dom from '../lib/dom';
+
+describe('testing App', () => {
+  let sandboxes;
+  let renderDateTime;
+
+  beforeEach(() => {
+    sandboxes = createSandbox();
+    renderDateTime = sandboxes.stub(Dom, 'renderDateTime');
+  });
+
+  afterEach(() => {
+    sandboxes.restore();
+  });
+
+  describe('testing App', () => {
+    it('should return Thu Nov 19 23:00', () => {
+      const app = new App();
+      app.render();
+      expect(renderDateTime.called).toBeTruthy();
+    });
+  });
+});
